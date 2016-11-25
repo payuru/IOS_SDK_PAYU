e9aee2f72de90caebc5f1d250a7ac00bf332c09a
Max <ixotdog@gmail.com>
2016-11-25 10:25:18 +0300

    init app 0.0.1

---
+++ Example/Pods/Target Support Files/IOS_SDK_PAYU/IOS_SDK_PAYU-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_IOS_SDK_PAYU : NSObject
+@end
+@implementation PodsDummy_IOS_SDK_PAYU
+@end
---
+++ Example/Pods/Target Support Files/Pods-IOS_SDK_PAYU_Example/Pods-IOS_SDK_PAYU_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_IOS_SDK_PAYU_Example : NSObject
+@end
+@implementation PodsDummy_Pods_IOS_SDK_PAYU_Example
+@end
